c87036f4b690b6c42db350a913ab47f864e7e0d6
Stefane Fermigier <sf@fermigier.com>
2020-10-03 19:57:19 +0200

    Init

---
+++ README.md
@@ -0,0 +1,18 @@
+### Hi there 👋
+
+My name is Stefane Fermigier, I trained and worked as a mathematician in in 80s and 90s, discovered free and open source software in the 90s (my first contact with Linux was with version 0.10 in December 1991), and switched to enterpreneurship in the 00s.
+
+I discovered Python around 1992, while working on my Thesis (a part of my Thesis was implemented using the PARI/GP computational arithmetic project, for which I made a Python binding).
+
+Here are some automated stats about my published Python projects
+([thanks Simon Willison!][sw-post]):
+
+<!--marker-->
+Total downloads: 1,109,189<br>
+Downloads/month: 19,298<br>
+Stars count: 1,676
+<!--end-->
+
+Most of these projects are created and supported by my comparny, [Abilian SAS](https://abilian.com/).
+
+[sw-post]: https://simonwillison.net/2020/Jul/10/self-updating-profile-readme/
---
+++ build_readme.py
@@ -0,0 +1,66 @@
+import sys
+
+import httpx
+from pprint import pprint
+
+README_TEMPLATE = """
+Total downloads: {total_dl:,}<br>
+Downloads/month: {total_dl_per_month:,}<br>
+Stars count: {total_stars:,}
+""".strip("\n")
+
+GITHUB_ACCOUNTS = [
+    "sfermigier",
+    "abilian",
+]
+
+PYTHON_PROJECTS = [
+    "abilian-core",
+    "abilian-sbe",
+    "olapy",
+    # "olapy-web",
+    "tentakel",
+    "ldif",
+    "abilian-crm-core",
+    "WhooshAlchemy",
+    "comt",
+]
+
+PEPYTECH_API_URL = "https://api.pepy.tech/api/projects/"
+GITHUB_API_URL = "https://api.github.com/"
+
+def main():
+    total_stars = total_dl = total_dl_per_month = 0
+    with httpx.Client() as client:
+        for project in PYTHON_PROJECTS:
+            data = client.get(PEPYTECH_API_URL + project).json()
+            pprint(data)
+            print("\n")
+            total_dl += data["total_downloads"]
+            total_dl_per_month += sum(data["downloads"].values())
+
+        for account in GITHUB_ACCOUNTS:
+            repos = client.get(GITHUB_API_URL + f"users/{account}/repos?per_page=100").json()
+            total_stars += sum(repo["stargazers_count"] for repo in repos)
+
+    text = README_TEMPLATE.format(
+        total_dl=total_dl,
+        total_dl_per_month=total_dl_per_month,
+        total_stars=total_stars,
+    )
+
+    with open("README.md") as fd:
+        lines = fd.read().splitlines(keepends=False)
+
+    marker_start = lines.index("<!--marker-->")
+    marker_end = lines.index("<!--end-->")
+    lines = lines[:marker_start+1] + [text] + lines[marker_end:]
+
+    with open("README.md", "w") as fd:
+        fd.write("\n".join(lines))
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
